d01d2c2dca2f7b4b30c7c5ad0411137a59d7b944
Jefferson0k <jefersoncovenas7@gmail.com>
2025-06-09 18:17:36 -0500

    Solo se pusieron botones que estan sin acciones

--- resources/js/pages/panel/TasasFijas/Facturas/Desarrollo/AddFacturas.vue
+++ resources/js/pages/panel/TasasFijas/Facturas/Desarrollo/AddFacturas.vue
@@ -1,6 +1,30 @@
 <template>
-    <h1>Add Facturas</h1>
+    <Toast />
+    <Toolbar class="mb-6">
+        <template #start>
+            <Button label="Nuevo" icon="pi pi-plus" severity="secondary" class="mr-2" @click="showToast" />
+            <Button label="Eliminar" icon="pi pi-trash" severity="secondary" @click="showToast" />
+        </template>
+        <template #end>
+            <Button label="Exportar" icon="pi pi-upload" severity="secondary" @click="showToast" />
+        </template>
+    </Toolbar>
 </template>
-<script setup lang="ts">
 
+<script setup lang="ts">
+import Toolbar from 'primevue/toolbar'
+import Button from 'primevue/button'
+import Toast from 'primevue/toast'
+import { useToast } from 'primevue/usetoast'
+
+const toast = useToast()
+
+const showToast = () => {
+    toast.add({
+        severity: 'info',
+        summary: 'Información',
+        detail: 'Aún se encuentra en desarrollo',
+        life: 3000
+    })
+}
 </script>
