72cb0a8bf0b6400f00d792ecdc08cc1c8639e957
Jefferson0k <jefersoncovenas7@gmail.com>
2025-06-11 17:59:05 -0500

    letras

--- database/factories/invoiceFactory.php
+++ database/factories/invoiceFactory.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-class InvoiceFactory extends Factory
+class invoiceFactory extends Factory
 {
     protected $model = Invoice::class;
 
