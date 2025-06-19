665220dda1acfcff200559ec9270120e9bf12d8b
Jefferson0k <jefersoncovenas7@gmail.com>
2025-06-19 10:19:37 -0500

    ok

--- database/migrations/2025_08_07_200133_add_email_verification_to_investors_table.php
+++ database/migrations/2025_08_07_200133_add_email_verification_to_investors_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('investors', function (Blueprint $table) {
+        Schema::table('customers', function (Blueprint $table) {
             $table->dropColumn('email_verified_at');
         });
     }
